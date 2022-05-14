0832db6478758ad341ea67843bb0a20161683229
Thalia <xi1le7@gmail.com>
2022-05-14 02:24:52 +0000

    first.commit

--- cm.php
+++ cm.php
@@ -1,6 +1,6 @@
 <?php
 $dom = new DOMDocument;
-$dom->loadHTMLFile("cm.html");
+$dom->loadHTMLFile(mb_convert_encoding("cm.html", 'HTML-ENTITIES', 'UTF-8'));
 $h2 = $dom->getElementsByTagName('h2');
 $i=0;
 $p = $dom->getElementsByTagName('p');
@@ -9,8 +9,8 @@ $p = $dom->getElementsByTagName('p');
 <!DOCTYPE html>
 <html>
 <head>
-   <title>My Website</title>
-  <!-- Latest compiled and minified CSS -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Mon blog</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
